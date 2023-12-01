e96c512e2162fffc2d969b5cd873cf6ae2752f0f
Alumno <Alumno@LAB1509-18>
2023-12-01 17:33:05 -0500

    Agregar archivos de proyecto.

---
+++ Lab12/Lab12/Models/TaskModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab12.Models
+{
+    public class TaskModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+
+        public TaskModel() {
+        }
+        //el modelo mapea la base de datos, viewmodel mapea la vista
+    }
+}
